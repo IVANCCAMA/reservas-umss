6f229c7f86a0357e0fd3169b7c16d744f555da79
IVANCCAMA <ccamaivan@gmail.com>
2024-03-28 19:19:36 -0400

    Add mapeo de arreglo de onbjetos en ListadoMateriasPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ExamplePage from './pages/ExamplePage/ExamplePage';
 import HomePage from './pages/HomePage/HomePage';
-import ListadoAmbientesPage from './pages/AmbientesPage/ListadoAmbientesPage/ListadoAmbientesPage';
 import ListadoMateriasPage from './pages/MateriasPage/ListadoMateriasPage/ListadoMateriasPage';
 import SubirMatariasPage from './pages/MateriasPage/SubirMatariasPage/SubirMatariasPage';
 
@@ -18,7 +17,7 @@ function App() {
             <Routes>
               <Route index element={<ExamplePage />} />
               <Route path="/home" element={<HomePage />} />
-              <Route path="/ambientes" element={<ListadoAmbientesPage />} />
+              <Route path="/home" element={<ListadoMateriasPage />} />
               <Route path="/materias" element={<ListadoMateriasPage />} />
               <Route path="/materias/subir-materia" element={<SubirMatariasPage />} />
             </Routes>
--- src/pages/MateriasPage/ListadoMateriasPage/ListadoMateriasPage.jsx
+++ src/pages/MateriasPage/ListadoMateriasPage/ListadoMateriasPage.jsx
@@ -1,20 +1,54 @@
-import './ListadoMateriasPage.scss'
+import './ListadoMateriasPage.scss';
 
 const ListadoMateriasPage = () => {
   // estados
 
   // variables
+  const materias = [
+    {
+      id: 1,
+      Materia: 'Matemáticas',
+      Nivel: 'Bachillerato',
+      Grupo: 'A',
+      Inscritos: 30,
+      Docentes: ['Juan Pérez', 'María López'],
+    },
+    {
+      id: 2,
+      Materia: 'Historia',
+      Nivel: 'Secundaria',
+      Grupo: 'B',
+      Inscritos: 25,
+      Docentes: ['Pedro García'],
+    },
+    {
+      id: 3,
+      Materia: 'Ciencias Naturales',
+      Nivel: 'Primaria',
+      Grupo: 'C',
+      Inscritos: 20,
+      Docentes: ['Ana Martínez'],
+    },
+    {
+      id: 4,
+      Materia: 'Literatura',
+      Nivel: 'Bachillerato',
+      Grupo: 'D',
+      Inscritos: 28,
+      Docentes: ['Luis Hernández', 'Sofía Ramírez'],
+    },
+  ];
 
   // logica | api
+  
 
   return (
-    <div className="container listado-materias">
+    <div className="container listado-materias p-5">
       <h2 className="text-start">Materias registradas</h2>
       <table className="table table-striped border border-1">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">ID</th>
             <th scope="col">Materia</th>
             <th scope="col">Nivel</th>
             <th scope="col">Grupo</th>
@@ -23,78 +57,23 @@ const ListadoMateriasPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>001</td>
-            <td>Introducción a la programación</td>
-            <td>A</td>
-            <td>G1</td>
-            <td>112</td>
-            <td>Vladimir Costas</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>002</td>
-            <td>Elementos de programación</td>
-            <td>C</td>
-            <td>G1</td>
-            <td>120</td>
-            <td>Leticia Blanco</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>003</td>
-            <td>Introducion a la programación</td>
-            <td>D</td>
-            <td>G2</td>
-            <td>89</td>
-            <td>Leticia Blanco</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>004</td>
-            <td>Cálculo II</td>
-            <td>B</td>
-            <td>G6A</td>
-            <td>58</td>
-            <td>Alex Bustillos</td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td>005</td>
-            <td>Taller de Simulación de sistemas</td>
-            <td>F</td>
-            <td>G1</td>
-            <td>25</td>
-            <td>Henry Frank</td>
-          </tr>
-          <tr>
-            <th scope="row">6</th>
-            <td>006</td>
-            <td>Redes avanzadas de computador...</td>
-            <td>H</td>
-            <td>G2</td>
-            <td>196</td>
-            <td>Leticia Blanco</td>
-          </tr>
-          <tr>
-            <th scope="row">7</th>
-            <td>007</td>
-            <td>Introducción a la programación</td>
-            <td>B</td>
-            <td>G3</td>
-            <td>95</td>
-            <td>Leticia Blanco</td>
-          </tr>
-          <tr>
-            <th scope="row">8</th>
-            <td>008</td>
-            <td>Dinámica de sistemas</td>
-            <td>D</td>
-            <td>G1</td>
-            <td>23</td>
-            <td>Jorge Orellana</td>
-          </tr>
+          {console.log(materias)}
+          {materias.map((materia, index) => {
+            return (
+              <tr key={index}>
+                <th scope="row">{materia.id}</th>
+                <td>{materia.Materia}</td>
+                <td>{materia.Nivel}</td>
+                <td>{materia.Grupo}</td>
+                <td>{materia.Inscritos}</td>
+                <td>
+                  {materia.Docentes.map((Docente) => (
+                    <div key={Docente}>{Docente}</div>
+                  ))}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
       {/* Botones de paginación */}
