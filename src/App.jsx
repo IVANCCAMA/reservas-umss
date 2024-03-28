c96872ce03535db7e1a649ec36910525e93f8016
IVANCCAMA <ccamaivan@gmail.com>
2024-03-27 21:21:45 -0400

    Add ejemplo de Componente Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ExamplePage from "./pages/ExamplePage/ExamplePage";
-import HomePage from "./pages/HomePage/HomePage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ExamplePage from './pages/ExamplePage/ExamplePage';
+import HomePage from './pages/HomePage/HomePage';
+import ListadoAmbientesPage from './pages/AmbientesPage/ListadoAmbientesPage/ListadoAmbientesPage';
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div className="App">
-      <Routes>
-        <Route index element={<ExamplePage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route index element={<ExamplePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/ambientes/lista" element={<ListadoAmbientesPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/pages/AmbientesPage/ListadoAmbientesPage/ListadoAmbientesPage.jsx
@@ -0,0 +1,9 @@
+const ListadoAmbientesPage = () => {
+  return (
+    <div>
+      <h2>Lista de ambientes</h2>
+      <div>Lista</div>
+    </div>
+  );
+};
+export default ListadoAmbientesPage;
