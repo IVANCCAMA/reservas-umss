50fe1bca2555079b1de7855edd4e86acc592588f
IVANCCAMA <ccamaivan@gmail.com>
2024-03-27 22:41:59 -0400

    Add base CargadoMatariasPage

---
+++ src/pages/MateriasPage/CargadoMatariasPage/CargadoMatariasPage.jsx
@@ -0,0 +1,9 @@
+const CargadoMatariasPage = () => {
+  return (
+    <div className="cargar-materias container">
+      <div>Cargar materias</div>
+    </div>
+  );
+};
+
+export default CargadoMatariasPage;
