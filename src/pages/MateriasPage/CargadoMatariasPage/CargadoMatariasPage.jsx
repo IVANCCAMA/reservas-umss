0fac059d9300e1b031a0197d9df78a1164799c06
IVANCCAMA <ccamaivan@gmail.com>
2024-03-27 22:46:42 -0400

    Change CargadoMateriasPAge por SubirMatariasPage

--- src/pages/MateriasPage/CargadoMatariasPage/CargadoMatariasPage.jsx
+++
@@ -1,9 +0,0 @@
-const CargadoMatariasPage = () => {
-  return (
-    <div className="cargar-materias container">
-      <div>Cargar materias</div>
-    </div>
-  );
-};
-
-export default CargadoMatariasPage;
