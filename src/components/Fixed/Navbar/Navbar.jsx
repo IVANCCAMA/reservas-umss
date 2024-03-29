908b26b94835f3213a5b2d5232bac036fce66f75
IVANCCAMA <ccamaivan@gmail.com>
2024-03-29 17:54:00 -0400

    Add logo en Navbar

--- src/components/Fixed/Navbar/Navbar.jsx
+++ src/components/Fixed/Navbar/Navbar.jsx
@@ -1,23 +1,26 @@
 /* import { Link } from "react-router-dom";
  */
+import logo from '../../../assets/Images/logoReserBit.png';
+
 const Navbar = () => {
   return (
     <div
-    className="w-100"
+      className="d-flex align-items-center"
       style={{
         position: 'fixed',
         width: '100%',
         height: '55px',
         backgroundColor: '#ffffff',
         border: '0.5px solid rgba(140, 134, 134, 0.6)',
-        zIndex: 1
+        zIndex: 1,
       }}
     >
-      {/* <div
-        style={{
-          position: 'fixed',
-        }}
-      ></div> */}
+      <img
+        className="img-fluid object-fit-cover"
+        src={logo}
+        alt="logo de aplicación"
+        style={{ width: '100px' }}
+      />
     </div>
   );
 };
