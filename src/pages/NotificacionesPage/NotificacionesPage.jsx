778e902c96caf73b3da59052a1d8f7286bfe0085
IVANCCAMA <ccamaivan@gmail.com>
2024-05-28 22:07:04 -0400

    Add api dado un user

--- src/pages/NotificacionesPage/NotificacionesPage.jsx
+++ src/pages/NotificacionesPage/NotificacionesPage.jsx
@@ -2,83 +2,48 @@ import { useEffect, useState } from 'react';
 import Table from '../../components/Table/Table';
 import Pagination from '../../components/Pagination/Pagination';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react/dist/iconify.js';
-
-const notificacionesTest = [
-  {
-    id_notificacion: 1,
-    fecha_notificacion: '12:30 28-05-2024',
-    descripcion: 'Ambiente deshabilitado por mantenimiento',
-    leido: false,
-  },
-  {
-    id_notificacion: 2,
-    fecha_notificacion: '13:45 28-05-2024',
-    descripcion: 'Ambiente deshabilitado por actualización',
-    leido: false,
-  },
-  {
-    id_notificacion: 3,
-    fecha_notificacion: '14:00 28-05-2024',
-    descripcion: 'Ambiente deshabilitado por fallo técnico',
-    leido: true,
-  },
-];
+import { useAppSelector } from '../../redux/app/hooks';
 
 const NotificacionesPage = () => {
+  const baseURL = import.meta.env.VITE_APP_DOMAIN;
+
+  // user
+  const user = useAppSelector((state) => state.auth.usuario);
   // estados
   const [notificaciones, setNotificaciones] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
 
   const loadNotificaciones = () => {
-    /* Borrar esto */
-    setNotificaciones(
-      notificacionesTest.map((not) => {
-        const rows = {
-          'Fecha y hora': not.fecha_notificacion,
-          Descripción: not.descripcion,
-        };
-        const leido = (
-          <div className="form-check">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value=""
-              id="flexCheckDefault"
-              checked={not.leido}
-            />
-          </div>
-        );
-        return {
-          ...rows,
-          Leído: leido,
-        };
-      }),
-    );
-
-    /* Cambiar api */
-    /* let apiUsuario = '/grupos/tablamaterias/1';
-    if (user.tipo_usuario !== 'ADMINISTRADOR') {
-      apiUsuario = `/grupos/tablamaterias/${user.id_usuario}`;
-    }
-
     axios
-      .get(`${baseURL}${apiUsuario}`)
+      .get(`${baseURL}/notificaciones/${user.id_usuario}`)
       .then((response) => {
         setNotificaciones(
           response.data.map((not) => {
-            return {
-              'Fecha y hora': not.fecha_notificacion,
+            const rows = {
+              'Fecha y hora': not.registro,
               Descripción: not.descripcion,
-              Leído: not.leido,
+            };
+            const leido = (
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+                />
+              </div>
+            );
+            return {
+              ...rows,
+              Leído: leido,
             };
           }),
         );
       })
       .catch((error) => {
         console.error('Error al obtener las notificaciones:', error);
-      }); */
+      });
   };
 
   useEffect(() => {
